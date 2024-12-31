27e1c221defa421b2e4c1862e2f608c234c1e943
srnyx <25808801+srnyx@users.noreply.github.com>
2024-12-31 18:52:43 -0500

    [IMAGE DITHER] Allow custom secondary color

--- Scripts/Image Dither/Image Dither.py
+++ Scripts/Image Dither/Image Dither.py
@@ -1,20 +1,23 @@
 from PIL import Image
 import numpy as np
 
-color = [150, 255, 0]
+primary_color = [150, 255, 0, 255]
+secondary_color = [0, 0, 0, 255]
 
 img = Image.open('input.png')
 dither = np.random.default_rng(seed=42).integers(0, 256, size=(img.height, img.width))
 img = np.array(img).astype(int)
 new_img = np.copy(img)
-color_alpha = color + [255]
 transparent = [0, 0, 0, 0]
 
 for y in range(img.shape[0]):  # loop thru y-values
     for x in range(img.shape[1]):  # loop thru x-values
+        if img[y, x, 3] == 0:
+            new_img[y, x] = transparent
+            continue
         gray = (img[y, x, 0] + img[y, x, 1] + img[y, x, 2]) / 3  # grayscale (average)
         dither_value = dither[y % dither.shape[0], x % dither.shape[1]]  # dither comparison value
-        new_img[y, x] = color_alpha if gray > dither_value else transparent
+        new_img[y, x] = primary_color if gray > dither_value else secondary_color
 
 #new_img = np.clip(new_img, 0, 255) # shouldnt be necessary since all values should be 0 or 255
 Image.fromarray(new_img.astype(np.uint8)).save('output.png')
