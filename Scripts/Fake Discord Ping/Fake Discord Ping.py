24458ff9df70c26b56757e8884a8be25f100784e
srnyx <25808801+srnyx@users.noreply.github.com>
2025-03-31 19:45:00 -0400

    [FDP] Add GIF support

--- Scripts/Fake Discord Ping/Fake Discord Ping.py
+++ Scripts/Fake Discord Ping/Fake Discord Ping.py
@@ -1,19 +1,60 @@
 # Adds a fake Discord ping to your inputted file (server icon)
 
 from PIL import Image
+from PIL import ImageSequence
 
 
-# Get fake ping and input
 ping = Image.open("ping.png")
-input_png = Image.open("input.png")
 
-# Resize ping to same size as input
-ping = ping.resize(input_png.size)
 
-# Paste the fake ping
-input_png.paste(ping, (0, 0), ping)
+def png():
+    # Get input
+    try:
+        input_png = Image.open("input.png")
+    except FileNotFoundError:
+        return False
 
-# Save the new icon
-input_png.save("output.png")
+    # Resize ping to same size as input
+    sized_ping = ping.resize(input_png.size)
 
-print("Fake ping added to input.png and saved as output.png")
+    # Paste the fake ping
+    input_png.paste(sized_ping, (0, 0), sized_ping)
+
+    # Save the new icon
+    input_png.save("output.png")
+
+    print("PNG | Fake ping added to input.png and saved as output.png")
+    return True
+
+
+def gif():
+    # Get frames
+    try:
+        frames = [frame.copy() for frame in ImageSequence.Iterator(Image.open("input.gif"))]
+    except FileNotFoundError:
+        return False
+
+    # Resize ping to same size as frames
+    sized_ping = ping.resize(frames[0].size)
+
+    # Paste the fake ping in each frame
+    new_frames = []
+    for frame in frames:
+        frame = frame.convert("RGBA")
+        frame.paste(sized_ping, (0, 0), sized_ping)
+        new_frames.append(frame)
+
+    # Save the new icon
+    new_frames[0].save("output.gif", save_all=True, append_images=new_frames[1:])
+
+    print("GIF | Fake ping added to input.gif and saved as output.gif")
+    return True
+
+
+# PNG
+if not png():
+    print("PNG | No input.png found")
+
+# GIF
+if not gif():
+    print("GIF | No input.gif found")
---
+++ Scripts/Fake Discord Ping/README.md
@@ -0,0 +1,7 @@
+# Fake Discord Ping
+
+You need to have Python installed to run this script!
+
+1. Put your server icon in this folder and name it `input.png` or `input.gif` (depending on if it's static or animated)
+2. Run the `run.sh` file
+3. Your output will either be `output.png` or `output.gif`, enjoy!
